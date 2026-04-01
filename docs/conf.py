5c2730e11baea3dfe448f4ea981551e074499ddf
issam.mhadhbi.dev <issam.mhadhbi.dev@gmail.com>
2026-04-01 23:18:20 +0100

    gg

--- docs/conf.py
+++ docs/conf.py
@@ -1,18 +1,36 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))  # include root package
 
+# Include root package
+sys.path.insert(0, os.path.abspath('..'))
+
+# Project info
 project = 'pydefoldsdk'
 author = 'Mhadhbi Issam'
-release = open("./../VERSION").read()
 
+# Read version safely
+version_file = os.path.abspath(os.path.join('..', 'VERSION'))
+if os.path.exists(version_file):
+    release = open(version_file).read().strip()
+else:
+    release = '0.0.0'  # fallback if VERSION file missing
+
+# Sphinx extensions
 extensions = [
     'sphinx.ext.autodoc',   # generate docs from docstrings
     'sphinx.ext.napoleon',  # support Google/NumPy docstrings
+    'myst_parser',           # enable Markdown support
 ]
+
+# Support Markdown files
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-html_theme = 'sphinx_rtd_theme'  # Read the Docs theme
+# HTML output
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -4,5 +4,6 @@ Welcome to pydefoldsdk's documentation!
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
+   :glob:
 
-   modules
+   *.md
