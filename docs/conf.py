a11ad67b88bba0108616f53a620f05e8ff8d0593
issam.mhadhbi.dev <issam.mhadhbi.dev@gmail.com>
2026-04-01 23:14:04 +0100

    GHH

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath('..'))  # include root package
 
 project = 'pydefoldsdk'
 author = 'Mhadhbi Issam'
-release = open("VERSION").read()
+release = open("./../VERSION").read()
 
 extensions = [
     'sphinx.ext.autodoc',   # generate docs from docstrings
